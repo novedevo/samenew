3a55a9d4d4f894ff23643a358ed4d04aae43a003
Devon Sawatsky <devon@nove.dev>
2025-03-28 06:51:44 +0000

    bitvec :)

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,102 @@
+use bitvec::prelude::*;
 use std::f32::consts::PI;
 
 pub struct SineWave {
     hz: f32,
-    sample_rate: usize,
-    samples: usize,
+    // sample_rate: usize,
+    // samples: usize,
     seconds: f32,
     cycles: f32,
 }
 
 impl SineWave {
-    fn new_from_sample_count(hz: f32, sample_rate: usize, samples: usize) -> Self {
-        let seconds = samples as f32 / sample_rate as f32;
+    // fn new_from_sample_count(hz: f32, sample_rate: usize, samples: usize) -> Self {
+    //     let seconds = samples as f32 / sample_rate as f32;
+    //     let cycles = hz * seconds;
+    //     Self {
+    //         hz,
+    //         sample_rate,
+    //         samples,
+    //         seconds,
+    //         cycles,
+    //     }
+    // }
+    fn new_from_seconds(hz: f32, seconds: f32) -> Self {
+        // let samples = sample_rate as f32 * seconds;
         let cycles = hz * seconds;
         Self {
             hz,
-            sample_rate,
-            samples,
+            // sample_rate,
+            // samples: samples.floor() as usize,
             seconds,
             cycles,
         }
     }
-    fn new_from_seconds(hz: f32, sample_rate: usize, seconds: f32) -> Self {
-        let samples = sample_rate as f32 * seconds;
-        let cycles = hz * seconds;
-        Self {
-            hz,
-            sample_rate,
-            samples: samples.floor() as usize,
-            seconds,
-            cycles,
-        }
-    }
-    fn new_from_cycles(hz: f32, sample_rate: usize, cycles: f32) -> Self {
+    fn new_from_cycles(hz: f32, cycles: f32) -> Self {
         let seconds = hz * cycles;
-        let samples = sample_rate as f32 * seconds;
+        // let samples = sample_rate as f32 * seconds;
         Self {
             hz,
-            sample_rate,
-            samples: samples.floor() as usize,
+            // sample_rate,
+            // samples: samples.floor() as usize,
+            seconds,
+            cycles,
+        }
+    }
+    fn new_from_cycles_and_seconds(cycles: f32, seconds: f32) -> Self {
+        let hz = cycles / seconds;
+        Self {
+            hz,
             seconds,
             cycles,
         }
     }
 
-    fn generate_samples(&self) -> Vec<f32> {
-        (0..self.samples).map(|s| s as f32).map(|sample_index| {
-            let way_through_cycle = sample_index / ((self.samples - 1) as f32 / self.cycles);
-            (way_through_cycle * 2.0 * PI).sin()
-        }).collect()
+    fn generate_samples(&self, sample_rate: usize) -> Vec<f32> {
+        let samples = (sample_rate as f32 * self.seconds).floor() as usize;
+        (0..samples)
+            .map(|sample_index| {
+                let way_through_cycle = sample_index as f32 / ((samples - 1) as f32 / self.cycles);
+                (way_through_cycle * 2.0 * PI).sin()
+            })
+            .collect()
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum AfskBit {
+    Mark,
+    Space,
+}
+
+impl From<AfskBit> for SineWave {
+    fn from(bit: AfskBit) -> Self {
+        let cycles = match bit {
+            AfskBit::Mark => 4.0,
+            AfskBit::Space => 3.0,
+        };
+        Self::new_from_cycles_and_seconds(cycles, 1.92 / 1000.0)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct AfskByte {
+    bits: [AfskBit; 8],
+}
+
+impl From<u8> for AfskByte {
+    fn from(byte: u8) -> Self {
+        let bits = byte
+            .view_bits::<Lsb0>()
+            .iter()
+            .by_vals()
+            .map(|bit| if bit { AfskBit::Mark } else { AfskBit::Space })
+            .collect::<Vec<AfskBit>>();
+        AfskByte {
+            bits: [
+                bits[0], bits[1], bits[2], bits[3], bits[4], bits[5], bits[6], bits[7],
+            ],
+        }
     }
 }
 
@@ -57,7 +106,7 @@ mod test {
 
     #[test]
     fn simple_sine() {
-        let sine = SineWave::new_from_cycles(1.0, 50, 1.0);
-        dbg!(sine.generate_samples());
+        let sine = SineWave::new_from_cycles(1.0, 1.0);
+        dbg!(sine.generate_samples(50));
     }
 }
